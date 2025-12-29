07f83cffd20abacbcbc3778ea126f3cb96357c51
adityayadav71 <aditya.yadav@dhdigital.co.in>
2025-12-29 19:58:19 +0530

    Initial Commit

---
+++ src/main/java/com/aditya/inventory/HighConcurrencyInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/inventory/HighConcurrencyInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
