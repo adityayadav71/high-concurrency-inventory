8afc1b0a14e364123eda4fb4dd3eab60c1087d35
adityayadav71 <aditya.yadav@dhdigital.co.in>
2026-01-03 20:06:48 +0530

    chore(db): setup PostgreSQL connection and docker-compose - Add docker-compose.yml to spin up Postgres 16 and pgAdmin 4. - Setup use of .env for secrets in docker-compose.yml and application.properties files. - Set Default Timezone to "Asia/Kolkata" In SpringBootApplication EntryPoint Method to avoid db connection issues on startup.

--- src/main/java/com/aditya/inventory/HighConcurrencyInventoryApplication.java
+++ src/main/java/com/aditya/inventory/HighConcurrencyInventoryApplication.java
@@ -1,5 +1,7 @@
 package com.aditya.inventory;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HighConcurrencyInventoryApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(HighConcurrencyInventoryApplication.class, args);
 	}
 
