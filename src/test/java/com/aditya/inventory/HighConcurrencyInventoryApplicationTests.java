92b75da7169011cb3007bc04106d8f762bc6ba64
adityayadav71 <aditya.yadav@dhdigital.co.in>
2026-01-12 21:27:09 +0530

    chore(db): update default timezone compatible with db in Test EntryPoint - updated the default timezone to Asia/Kolkata in the main application test entry point to prevent postgres db connection failure.

--- src/test/java/com/aditya/inventory/HighConcurrencyInventoryApplicationTests.java
+++ src/test/java/com/aditya/inventory/HighConcurrencyInventoryApplicationTests.java
@@ -1,10 +1,16 @@
 package com.aditya.inventory;
 
+import java.util.TimeZone;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HighConcurrencyInventoryApplicationTests {
+
+	static {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	@Test
 	void contextLoads() {
